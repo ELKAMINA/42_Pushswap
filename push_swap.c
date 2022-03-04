9a352a184d4b0362aa8ceb0220bfc2b519f8bc97
Amina EL KHATTABI <ael-khat@e2r9p10.clusters.42paris.fr>
2022-03-04 15:36:39 +0100

    OK good

---
+++ push_swap.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-khat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 17:40:38 by ael-khat          #+#    #+#             */
+/*   Updated: 2022/03/04 14:50:01 by ael-khat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_exit(t_list *list)
+{
+	if (!list)
+	{
+		write(2, "Error malloc\n", 14);
+		exit(0);
+	}
+}
+
+void	create_nodes(t_list *head_a, int n, t_list *current_a, char *argv[])
+{
+	int	i;
+
+	i = 3;
+	while (i <= n)
+	{
+		current_a = ft_lstnew((int)ft_atol(argv[i]));
+		ft_exit(current_a);
+		ft_lstadd_back(&head_a, current_a);
+		i++;
+	}
+}
+
+t_list	*create_a(int argc, char *argv[])
+{
+	int		n;
+	t_list	*head_a;
+	t_list	*current_a;
+
+	n = argc - 1;
+	if (n == 0)
+		return (NULL);
+	if (n == 1)
+	{
+		head_a = ft_lstnew((int)ft_atol(argv[1]));
+		ft_exit(head_a);
+		head_a -> next = NULL;
+		head_a -> previous = NULL;
+	}
+	else
+	{
+		head_a = ft_lstnew((int)ft_atol(argv[1]));
+		ft_exit(head_a);
+		current_a = ft_lstnew((int)ft_atol(argv[2]));
+		ft_exit(current_a);
+		head_a -> next = current_a;
+		current_a -> previous = head_a;
+		create_nodes(head_a, n, current_a, argv);
+	}
+	return (head_a);
+}
+
+void	algos(t_list *head_a, t_list *head_b, int argc)
+{
+	if (argc == 3)
+		sorting_two(&head_a);
+	else if (argc == 4)
+		sorting_three(&head_a);
+	else if (argc == 5)
+		sorting_four(&head_a, &head_b);
+	else if (argc == 6)
+		sorting_five(&head_a, &head_b);
+	else
+		sorting_above_five(&head_a, &head_b);
+	ft_lstclear(&head_a);
+}
+
+int	main(int argc, char *argv[])
+{
+	int		n;
+	t_list	*head_a;
+	t_list	*head_b;
+
+	head_a = create_a(argc, argv);
+	head_b = NULL;
+	n = argc - 1;
+	if (argc == 1)
+	{
+		write(2, "Error\n", 7);
+		return (0);
+	}
+	if (check_dup(argv, n) == 0 || check_char(argv) == 0
+		|| check_sp(argv) == 0 || check_dsi(argv) == 0)
+	{
+		write (2, "Error\n", 7);
+		return (0);
+	}
+	else
+		algos(head_a, head_b, argc);
+	return (0);
+}
